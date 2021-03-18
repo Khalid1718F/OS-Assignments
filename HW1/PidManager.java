19d28895106e5b481caf78d00a41bf77b6cec2d9
Khalid Ahmed <63851527+Khalid718@users.noreply.github.com>
2021-03-18 13:15:00 -0400

    Update PidManager.java

--- HW1/PidManager.java
+++ HW1/PidManager.java
@@ -13,18 +13,25 @@ private static int[] pids;
  memory to store the pids.*/
 public static int allocatMap()
 {
+  //The pids variable is a assigned to the range of static variables between
+  //the initially declared minimum and maximum values. 
   pids = new int[MAX_PID - MIN_PID];
+  //An if clause takes place to check if the pids variable is in use.
   if (pids == null)
   {
+    //If the Pids Variable is not in use then the memory allocation
+   //process has failed and a sentinel value of -1 will be returned.
    System.out.println("Memory allocation failed.");
    return -1;
   }
-
+  //The length of the pid is iterated over using a for loop.
   for (int i = 0; i < pids.length; i++)
   {
+   //If the pid and index position i has a value of 0 then we can allocate memory.
    pids[i] = 0;
   }
   System.out.println("Memory allocated successfully.");
+  //Once memory has been allocated successfully then return value will be 1.
   return 1;
 }
 
@@ -32,28 +39,32 @@ public static int allocatMap()
 // This method allocates and returns a pid.
 public static int allocatPID()
 {
+  //If the value of pids is still null then it will not initialize the manager.
   if (pids == null)
   {
    System.out.println("PID Manager is not initialized ");
    return -1;
   }
-  
+  //A new pidNum variable is created and set to a value of -1.
   int pidNum = -1;
-  
+  //The pid value is iterated over again and if it is equal to 0 at index [i],
+  //then it will be assigned to the value 1 at index i and i will be added to 
+  //the MIN_PID value and assigned to the variable that was created earlier pidNum.
   for (int i = 0; i < pids.length; i++)
   {
-   if (pids[i] == 0)
-   {
-    pids[i] = 1;
-    pidNum = i + MIN_PID;
-    break;
-   }
+    if (pids[i] == 0) {
+      pids[i] = 1;
+      pidNum = i + MIN_PID;
+      break;
+    }
   }
+  //If the pidNum is equal to -1  then that means the pid cannot be allocated.
   if (pidNum == -1)
   {
-   System.out.println("Unable to allocat PID");
-   return -1;
+    System.out.println("Unable to allocat PID");
+    return -1;
   }
+  //Otherwise it will return the value of the pid which was allocated.
   System.out.println("Allocate PID :" + pidNum);
   return pidNum;
 }
@@ -82,5 +93,3 @@ public static void releasePID(int pidNum)
   pids[newPid] = 0;
 }
 }
-
-
